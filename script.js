2c8943c54de9fa42b494881ef06cc7b508c4b453
padfoot9445 <84330201+padfoot9445@users.noreply.github.com>
2024-03-08 10:54:56 +0000

    Added dropdown

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function DropdownButton()
+var dropdownIsDisplayed = false;
+function ToggleDropdown()
 {
-    alert("Test");
+    const Dropdown = document.getElementById("projects-dropdown-list");
+    function ToggleDropdownInner()
+    {
+        dropdownIsDisplayed = !dropdownIsDisplayed
+        return dropdownIsDisplayed;
+    }
+    function SetDropdownDisplay(toBeVisible)
+    {
+        Dropdown.style.display = toBeVisible? "block": "none";
+    }
+    SetDropdownDisplay(ToggleDropdownInner());
 }
