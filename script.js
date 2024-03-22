8b6cf40fe802ceeb194d7e3cf7fe904ce4072dc8
padfoot9445 <84330201+padfoot9445@users.noreply.github.com>
2024-03-22 10:55:19 +0000

    Attempted to make a 50/50 rickroll; random functio is not working in js

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function ToggleDropdown()
     }
     SetDropdownDisplay(ToggleDropdownInner());
 }
+
+function Gamble()
+{
+    const e = Math.random();
+    console.log(e);
+    if( e > 0.5 )
+    {
+        window.location.replace("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjNjaf82IeFAxU7UUEAHZH3DEsQwqsBegQIEBAG&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449");
+    }
+    else
+    {
+        alert("Safe...for now");
+    }
+}
